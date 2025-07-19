bdb5b0f300a02c89dca4664ca7094551c6c36f38
Ritik Raj <ritikraj2186@gmail.com>
2025-07-19 14:29:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', () => {
+  const nav = document.getElementById('navbar');
+  if (window.scrollY > 10) {
+    nav.classList.add('scrolled');
+  } else {
+    nav.classList.remove('scrolled');
+  }
+});
